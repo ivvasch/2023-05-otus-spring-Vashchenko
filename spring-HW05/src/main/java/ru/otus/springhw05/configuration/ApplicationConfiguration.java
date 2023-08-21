70dd7b4ffc0e27bbb444f868a7c16e5d3cd0f1e8
ivaschenko <vaschenko@omnicomm.ru>
2023-08-21 21:28:32 +0300

    Revert "uncomm"
    This reverts commit 10e54ea10e59885c3472a1115cc65562c90d5b51.

--- spring-HW05/src/main/java/ru/otus/springhw05/configuration/ApplicationConfiguration.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.springhw05.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApplicationConfiguration {
-}
--- spring-HW06/src/main/java/ru/otus/springhw06/configuration/ApplicationConfiguration.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.springhw06.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApplicationConfiguration {
-}
